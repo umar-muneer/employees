7a8d8372c0deba2be015774cf6551cf8e9619c43
Umar Muneer <umar.muneer@zameen.com>
2019-12-10 12:57:23 +0500

    added a sample api

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const expressBunyan = require("express-bunyan-logger");
+const app = express();
+
+const employees = [{
+    id: 1,
+    employeeName: "Umar Muneer",
+    employeeTitle: "SA",
+    employeeEmail: "umar.muneer@gmail.com",
+    employeePhone: "+923005679588"
+}, {
+    id: 2,
+    employeeName: "Usman Muneer",
+    employeeTitle: "SSE",
+    employeeEmail: "usman.muneer@gmail.com",
+    employeePhone: "+923015679589"
+}];
+
+app.use(expressBunyan());
+app.get("/employees", (req, res) => {
+    const pageLimit = req.query.pageLimit;
+    const bodyLimit = req.query.bodyLimit;
+    res.json(employees);
+});
+
+app.get("/employees/:id", (req, res) => {
+    const employee = employees.find(employee => employee.id === req.param.id);
+    res.json(employee);
+});
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`listening on ${port}`));
